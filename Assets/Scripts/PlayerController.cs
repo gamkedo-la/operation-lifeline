bca3fddd6c65887efc110bac12fafde88b93f51e
lordrahvin <lordrahvin@gmail.com>
2019-12-09 23:18:52 -0800

    Revert "Refactored player Controller and setup new PlayerInput script.  In progress."
    This reverts commit 6a8971d65b673456189389d0eb0196bf78d77137.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,44 +4,88 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float thrust = 10f;
-    public float leftPlayerTorque = -1f;
-    public float rightPlayerTorque = 1f;
-	ConstantForce2D constantForce2D;
-	[SerializeField] private PlayerEffects playerEffects;
 
-	void Start()
+    public float thrust = 10f;
+
+    public float player1Torque = -1f;
+    public float player2Torque = 1f;
+	public MeshRenderer player1Thruster;
+	public MeshRenderer player2Thruster;
+	public AudioSource thrusterSFX;
+	[Range(0f,100f)] public float thrusterVolume = 50f;
+	ConstantForce2D constantForce2D;
+  
+
+
+    // Use this for initialization
+    void Awake()
     {
         constantForce2D = GetComponent<ConstantForce2D>();
+		if (thrusterSFX) 
+		{
+			thrusterSFX.volume = 0f;
+			thrusterSFX.Play();
+		}
 	}
 
-  
+    // Update is called once per frame
     void FixedUpdate()
     {
-		PlayerInput.CheckInput();
-		float currentThrust = 0f;
-		float currentTorque = 0f;
-		if (PlayerInput.LeftThrust) 
-		{
-			currentTorque += leftPlayerTorque;
-			currentThrust += thrust;
-		}
-		if (PlayerInput.RightThrust) 
-		{
-			currentTorque += rightPlayerTorque;
-			currentThrust += thrust; 
-		}
+        /*if (Input.GetMouseButton(0) && Input.GetMouseButton(1) == false)
+        {
+            constantForce2D.relativeForce = new Vector2(0f, thrust);
+            constantForce2D.torque = player1Torque;
+        }else if (Input.GetMouseButton(1) && Input.GetMouseButton(0) == false)
+        {
+            constantForce2D.relativeForce = new Vector2(0f, thrust);
+            constantForce2D.torque = player2Torque;
+        }else if (Input.GetMouseButton(0) && Input.GetMouseButton(1))
+        {
+            constantForce2D.relativeForce = new Vector2(0f, thrust + thrust);
+            constantForce2D.torque = 0f;
+        }else if(Input.GetMouseButton(0) == false && Input.GetMouseButton(1) == false)
+        {
+            constantForce2D.relativeForce = new Vector2(0f, 0f);
+            constantForce2D.torque = 0f;
+        }*/
 
-		constantForce2D.relativeForce = new Vector2(0f, currentThrust);
-		constantForce2D.torque = currentTorque;
-		SetThrusters(PlayerInput.LeftThrust, PlayerInput.RightThrust);
+
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.RightShift) == false)
+        {
+            constantForce2D.relativeForce = new Vector2(0f, thrust);
+            constantForce2D.torque = player1Torque;
+			SetThrusters(true, false);
+		}
+        else if (Input.GetKey(KeyCode.RightShift) && Input.GetKey(KeyCode.LeftControl) == false)
+        {
+            constantForce2D.relativeForce = new Vector2(0f, thrust);
+            constantForce2D.torque = player2Torque;
+			SetThrusters(false, true);
+		}
+        else if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.RightShift))
+        {
+            constantForce2D.relativeForce = new Vector2(0f, thrust + thrust);
+            constantForce2D.torque = 0f;
+			SetThrusters(true, true);
+		}
+        else if ((Input.GetKey(KeyCode.LeftControl) == false && Input.GetKey(KeyCode.RightShift) == false))
+        {
+            constantForce2D.relativeForce = new Vector2(0f, 0f);
+            constantForce2D.torque = 0f;
+			SetThrusters(false, false);
+		}
     }
 
-	private void SetThrusters(bool leftPlayerThrusting, bool rightPlayerThrusting)
+	private void SetThrusters(bool plyrOneThrusting, bool plyrTwoThrusting)
 	{
-		if (playerEffects)
+		if (player1Thruster) { player1Thruster.enabled = plyrOneThrusting; }
+		if (player2Thruster) { player2Thruster.enabled = plyrTwoThrusting; }
+		float engineBurn = 0f;
+		if (plyrOneThrusting) { engineBurn = engineBurn + 0.5f; }
+		if (plyrTwoThrusting) { engineBurn = engineBurn + 0.5f; }
+		if (thrusterSFX)
 		{
-			playerEffects.SetThrusters(leftPlayerThrusting, rightPlayerThrusting);
+			thrusterSFX.volume = thrusterVolume * engineBurn / 100;
 		}
 	}
 }
