d8399ecb8a41f0c7abf1965e44b8f3a17b52292a
lordrahvin <lordrahvin@gmail.com>
2019-12-09 23:21:55 -0800

    no message

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,88 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float thrust = 10f;
-
-    public float player1Torque = -1f;
-    public float player2Torque = 1f;
-	public MeshRenderer player1Thruster;
-	public MeshRenderer player2Thruster;
-	public AudioSource thrusterSFX;
-	[Range(0f,100f)] public float thrusterVolume = 50f;
+    public float leftPlayerTorque = -1f;
+    public float rightPlayerTorque = 1f;
 	ConstantForce2D constantForce2D;
-  
+	[SerializeField] private PlayerEffects playerEffects;
 
-
-    // Use this for initialization
-    void Awake()
+	void Start()
     {
         constantForce2D = GetComponent<ConstantForce2D>();
-		if (thrusterSFX) 
-		{
-			thrusterSFX.volume = 0f;
-			thrusterSFX.Play();
-		}
 	}
 
-    // Update is called once per frame
+  
     void FixedUpdate()
     {
-        /*if (Input.GetMouseButton(0) && Input.GetMouseButton(1) == false)
-        {
-            constantForce2D.relativeForce = new Vector2(0f, thrust);
-            constantForce2D.torque = player1Torque;
-        }else if (Input.GetMouseButton(1) && Input.GetMouseButton(0) == false)
-        {
-            constantForce2D.relativeForce = new Vector2(0f, thrust);
-            constantForce2D.torque = player2Torque;
-        }else if (Input.GetMouseButton(0) && Input.GetMouseButton(1))
-        {
-            constantForce2D.relativeForce = new Vector2(0f, thrust + thrust);
-            constantForce2D.torque = 0f;
-        }else if(Input.GetMouseButton(0) == false && Input.GetMouseButton(1) == false)
-        {
-            constantForce2D.relativeForce = new Vector2(0f, 0f);
-            constantForce2D.torque = 0f;
-        }*/
+		PlayerInput.CheckInput();
+		float currentThrust = 0f;
+		float currentTorque = 0f;
+		if (PlayerInput.LeftThrust) 
+		{
+			currentTorque += leftPlayerTorque;
+			currentThrust += thrust;
+		}
+		if (PlayerInput.RightThrust) 
+		{
+			currentTorque += rightPlayerTorque;
+			currentThrust += thrust; 
+		}
 
-
-        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.RightShift) == false)
-        {
-            constantForce2D.relativeForce = new Vector2(0f, thrust);
-            constantForce2D.torque = player1Torque;
-			SetThrusters(true, false);
-		}
-        else if (Input.GetKey(KeyCode.RightShift) && Input.GetKey(KeyCode.LeftControl) == false)
-        {
-            constantForce2D.relativeForce = new Vector2(0f, thrust);
-            constantForce2D.torque = player2Torque;
-			SetThrusters(false, true);
-		}
-        else if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.RightShift))
-        {
-            constantForce2D.relativeForce = new Vector2(0f, thrust + thrust);
-            constantForce2D.torque = 0f;
-			SetThrusters(true, true);
-		}
-        else if ((Input.GetKey(KeyCode.LeftControl) == false && Input.GetKey(KeyCode.RightShift) == false))
-        {
-            constantForce2D.relativeForce = new Vector2(0f, 0f);
-            constantForce2D.torque = 0f;
-			SetThrusters(false, false);
-		}
+		constantForce2D.relativeForce = new Vector2(0f, currentThrust);
+		constantForce2D.torque = currentTorque;
+		SetThrusters(PlayerInput.LeftThrust, PlayerInput.RightThrust);
     }
 
-	private void SetThrusters(bool plyrOneThrusting, bool plyrTwoThrusting)
+	private void SetThrusters(bool leftPlayerThrusting, bool rightPlayerThrusting)
 	{
-		if (player1Thruster) { player1Thruster.enabled = plyrOneThrusting; }
-		if (player2Thruster) { player2Thruster.enabled = plyrTwoThrusting; }
-		float engineBurn = 0f;
-		if (plyrOneThrusting) { engineBurn = engineBurn + 0.5f; }
-		if (plyrTwoThrusting) { engineBurn = engineBurn + 0.5f; }
-		if (thrusterSFX)
+		if (playerEffects)
 		{
-			thrusterSFX.volume = thrusterVolume * engineBurn / 100;
+			playerEffects.SetThrusters(leftPlayerThrusting, rightPlayerThrusting);
 		}
 	}
 }
---
+++ Assets/Scripts/PlayerSounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+
+	[SerializeField] private AudioSource thrusterAudioSource;
+	[SerializeField] private AudioClip thrusterSound;
+	[Range(0f, 100f)] public float thrusterVolume = 50f;
+
+
+	private void Awake()
+	{
+		if (thrusterAudioSource)
+		{
+			if (thrusterSound) { thrusterAudioSource.clip = thrusterSound; }
+			thrusterAudioSource.volume = 0f;
+			thrusterAudioSource.Play();
+		}
+	}
+
+	public void SetVolume(float percent)
+	{
+		thrusterAudioSource.volume = thrusterVolume * percent;
+	}
+
+}
