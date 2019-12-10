f46280d195d16f4749b2e75aa95fcf3248fea4b2
lordrahvin <lordrahvin@gmail.com>
2019-12-10 01:00:27 -0800

    Created a new scene as a sort of mockup to how I picture the key selection scene at game start.

---
+++ Assets/ScrollingText.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingText : MonoBehaviour
+{
+	[Header("Scrolling Text")]
+	private bool scrolling = true;
+	private float scrollingSpeed = 0.25f;
+	private float arrivalThreshold = 0.1f;
+	[SerializeField] Transform startPoint;
+	[SerializeField] Transform endPoint;
+	private float timer = 0;
+	private float timerStopValue = 0.78f;
+	private float timerStartValue = 0.25f;
+	private float percent = 2f;
+	private bool stopped = false;
+	void Update()
+    {
+		ScrollText();
+    }
+
+	void ScrollText()
+	{
+		if (scrolling)
+		{
+			if (!stopped)
+			{
+				transform.position += Vector3.up * (Time.deltaTime * scrollingSpeed * percent);
+				startPoint.position += Vector3.down * (Time.deltaTime * scrollingSpeed * percent);
+				timer = timer + (Time.deltaTime * scrollingSpeed * percent);
+				if (timer >= (timerStopValue)) { stopped = true; timer = (timerStopValue); }
+			}
+			else if (stopped)
+			{
+				timer = timer - (Time.deltaTime * scrollingSpeed * percent);
+				if (timer <= (timerStartValue)) { stopped = false; timer = 0f; }
+			}
+
+			if (endPoint.position.y > (startPoint.position.y - arrivalThreshold))
+			{
+				scrolling = false;
+			}
+		}
+	}
+}
