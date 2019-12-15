ca0f1a6a0eb653ee2bbf0a125b02ca52a29bc4fd
stebssbets <stebssbets@yahoo.com>
2019-12-15 14:56:47 +0700

    WIP: Game over text when death occurs. Basic layout is there, but the objects aren't hooked up correctly. Need to teach in 3 minutes

---
+++ Assets/GameOverText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverText : MonoBehaviour
+{
+    public Transform gameOverText;
+    private Animator gameOverTextAnimator;
+    private GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverText = this.gameObject.transform.GetChild(0);
+
+        GameObject.Find("Player");
+
+        gameOverTextAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = player.transform.position;
+    }
+
+    public void ActivateAnimator()
+    {
+        gameOverTextAnimator.enabled = true;
+        Debug.Log("inside ActivateChild");
+    }
+}
