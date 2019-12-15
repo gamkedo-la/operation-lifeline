1c079e086bd50ad26d808f525b3204da0697a8f8
lordrahvin <lordrahvin@gmail.com>
2019-12-15 09:51:29 -0800

    Renamed asteroids to SpaceRocks.  Made the SpaceRocks rotate at random speeds and directions.

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
 	[SerializeField] private bool rotating = true;
-	[SerializeField] private float rotationSpeed = 1f;
+	[SerializeField] private bool weightedToward0 = true;
+	[SerializeField] private float minRrotationSpeed = -0.3f;
+	[SerializeField] private float maxRrotationSpeed = 0.3f;
+	private float rotationSpeed = 0f;
 
+	private void Start()
+	{
+		rotationSpeed = Random.Range(minRrotationSpeed, maxRrotationSpeed);
+		if (weightedToward0 && Random.Range(0,100)<50) { rotationSpeed = rotationSpeed * 0.5f; }
+	}
 	void Update()
     {
 		if (rotating) { Rotate(rotationSpeed * Time.deltaTime); }
