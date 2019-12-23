4f088f257d91fbb83745ebae63f7c645b070424e
Brian Boucher <junkyardconcierge@gmail.com>
2019-12-23 07:06:47 -0600

    Implemented Particle Effects for the thrusters.  WIP still.

--- Assets/Scripts/Player/PlayerEffects.cs
+++ Assets/Scripts/Player/PlayerEffects.cs
@@ -5,18 +5,50 @@ using UnityEngine;
 public class PlayerEffects : MonoBehaviour
 {
 
-	[SerializeField] private MeshRenderer leftPlayerThruster;
-	[SerializeField] private MeshRenderer rightPlayerThruster;
-	[SerializeField] private PlayerSounds playerSounds;
+    [SerializeField] private MeshRenderer leftPlayerThruster;
+    [SerializeField] private MeshRenderer rightPlayerThruster;
+    [SerializeField] private GameObject leftThrusterPFX;
+    [SerializeField] private GameObject rightThrusterPFX;
+    [SerializeField] private PlayerSounds playerSounds;
+
+    private ParticleSystem left_pfx;
+    private ParticleSystem right_pfx;
+
+    public void Start()
+    {
+        left_pfx = leftThrusterPFX.GetComponent(typeof(ParticleSystem)) as ParticleSystem;
+        right_pfx = rightThrusterPFX.GetComponent(typeof(ParticleSystem)) as ParticleSystem;
+        left_pfx.enableEmission = false;
+        right_pfx.enableEmission = false;
+    }
 
 	public void SetThrusters(bool leftPlayerThrusting, bool rightPlayerThrusting)
 	{
 		if (leftPlayerThruster) { leftPlayerThruster.enabled = leftPlayerThrusting; }
 		if (rightPlayerThruster) { rightPlayerThruster.enabled = rightPlayerThrusting; }
 		float engineBurn = 0.06f;
-		if (leftPlayerThrusting) { engineBurn = engineBurn + 0.47f; }
-		if (rightPlayerThrusting) { engineBurn = engineBurn + 0.47f; }
-		if (playerSounds)
+
+		if (leftPlayerThrusting)
+        {
+            engineBurn = engineBurn + 0.47f;
+            left_pfx.enableEmission = true;
+        }
+        else
+        {
+            left_pfx.enableEmission = false;
+        }
+
+        if (rightPlayerThrusting)
+        {
+            engineBurn = engineBurn + 0.47f;
+            right_pfx.enableEmission = true;
+        }
+        else
+        {
+            right_pfx.enableEmission = false;
+        }
+
+        if (playerSounds)
 		{
 			playerSounds.SetVolume(engineBurn / 100);
 		}
