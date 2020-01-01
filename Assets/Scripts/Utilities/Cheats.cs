29665cbd6e6b140c99a1a936419228620306f6a1
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2020-01-01 09:23:38 -0500

    Added a "Cheat Menu" for faster testing. Infinite health toggle.

---
+++ Assets/Scripts/Utilities/Cheats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheats : MonoBehaviour
+{
+
+    private GameObject player;
+    private PlayerController playerController;
+    private PlayerHealth playerHealth;
+    public bool infiniteHealth = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = FindObjectOfType<PlayerController>().gameObject;
+        playerHealth = player.GetComponent<PlayerHealth>();
+        playerController = player.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player == null)
+        {
+            player = FindObjectOfType<PlayerController>().gameObject;
+        }
+    }
+
+    public void InfiniteHealth()
+    {
+        if (infiniteHealth)
+        {
+            infiniteHealth = false;
+            playerHealth.enabled = true;
+        }
+        else
+        {
+            infiniteHealth = true;
+            playerHealth.enabled = false;
+        }
+    }
+}
