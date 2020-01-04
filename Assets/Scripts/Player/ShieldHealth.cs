df7222f190de1ad5827be67f0df868e9079bf71b
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-01-04 13:39:09 -0500

    Shield blocks damage and is destroyed after health is gone.

---
+++ Assets/Scripts/Player/ShieldHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldHealth : MonoBehaviour, IDamageable
+{
+
+    [SerializeField]
+    [Range(1,50)] private int health = 20;
+
+    private PlayerHealth playerHealth;
+
+    private void Update()
+    {
+        if(playerHealth == null)
+        {
+            playerHealth = GetComponentInParent<PlayerHealth>();
+            playerHealth.enabled = false;
+        }
+        
+    }
+
+    public void Die()
+    {
+        playerHealth.enabled = true;
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Debug.Log("shield takes" + damage + "damage");
+        health -= damage;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+}
