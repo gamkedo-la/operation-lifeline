5e599466df1043ec062a30ef428faddd0b20a906
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-01-04 12:46:41 -0500

    Shield Pickup instatiates Shield Prefab around Player.

---
+++ Assets/Scripts/Environment Mechanics/ShieldPickUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldPickUp : Collectibles
+{
+    public GameObject shieldPrefab;
+    public Transform shipTransform;
+
+    protected override void OnTriggerEnter2D(Collider2D player)
+    {
+        base.OnTriggerEnter2D(player);
+        shieldPrefab = Instantiate(shieldPrefab) as GameObject;
+        shieldPrefab.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
+        shieldPrefab.transform.position = shipTransform.position;
+        Destroy(gameObject);
+    }
+}
