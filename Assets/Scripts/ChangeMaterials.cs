331f40e5ca012d995e523ec9dee4b3a48a699c0a
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2020-01-13 13:03:06 -0500

    The paint job can be changed in the Ship Customization Scene (doesn't transfer to game yet) WIP

---
+++ Assets/Scripts/ChangeMaterials.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterials : MonoBehaviour
+{
+
+    [SerializeField]
+    private MeshRenderer modelMeshRenderer;
+
+    [SerializeField]
+    private Material[] materials;
+
+    private int materialIndex = 0;
+
+    public void RaiseMaterialIndex()
+    {
+        if(materialIndex < materials.Length)
+        {
+            materialIndex += 1;
+        }
+        else
+        {
+            materialIndex = 0;
+        }
+        ChangeMaterial();
+    }
+    public void LowerMaterialIndex()
+    {
+        if (materialIndex > 0)
+        {
+            materialIndex -= 1;
+        }
+        else
+        {
+            materialIndex = materials.Length;
+        }
+        ChangeMaterial();
+    }
+
+
+    public void ChangeMaterial()
+    {
+        modelMeshRenderer.material = materials[materialIndex];
+    }
+}
