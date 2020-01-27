cfdb0f064ed172fd2cbb7464290c9f9f0b28ffe9
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2020-01-27 13:22:57 -0500

    Added temporary script LevelLoader to test loading screen without touching the GameManager.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelLoader : MonoBehaviour
+{
+    //This is a temporary script to test loading mechanics.  This will be added to where level loading and scene management is handled 
+    //within the game architecture. 
+    // Start is called before the first frame update
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            Debug.Log(progress);
+
+            yield return null;
+        }
+    }
+
+}
