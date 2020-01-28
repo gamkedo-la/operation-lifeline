0e67ec7608ab67f29de983c3e3a648af50fb0f77
lordrahvin <lordrahvin@gmail.com>
2020-01-28 08:40:40 -0800

    Prefabbed some stuff in the Main Scene.  Worked on the fading in and out, still WIP.  Making a general Fader script.

---
+++ Assets/Fader.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+{
+	[SerializeField] private bool faded = false;
+	[SerializeField] private float defaultFadeTime = 3f;
+	private bool fadeStatusInMemory = false;
+	private bool fadingInProgress = false;
+	private Image target;
+
+	private void Start()
+	{
+		target = gameObject.GetComponent<Image>();
+	}
+
+	public void BeginFadeIn() { BeginFadeIn(defaultFadeTime); }
+	public void BeginFadeIn(float duration)
+	{
+		if (target)
+		{
+			object durObj = duration as object;
+			StartCoroutine("FadeIn", durObj);
+		}
+	}
+
+	public void BeginFadeOut() { BeginFadeOut(defaultFadeTime); }
+	public void BeginFadeOut(float duration)
+	{
+		if (target)
+		{
+			object durObj = duration as object;
+			StartCoroutine("FadeOut", durObj);
+		}
+		
+	}
+
+	
+	
+
+	private IEnumerator FadeIn(object durObj)
+	{
+		float duration = (float)durObj;
+		Color panelColor = target.color;
+		float alpha = 0f;
+		float targetAlpha = 1f;
+		for (float i = 0f; i < duration; i += Time.deltaTime)
+		{
+			panelColor = new Color(panelColor.r, panelColor.g, panelColor.b, alpha);
+			alpha = alpha + (1 / duration) * Time.deltaTime;
+			alpha = Mathf.Clamp(alpha, 0f, targetAlpha);
+			yield return null;
+		}
+		
+	}
+
+	private IEnumerator FadeOut(object durObj)
+	{
+		float duration = (float)durObj;
+		Color targetColor = target.color;
+		float alpha = target.color.a;
+		for (float i = 0f; i < duration; i += Time.deltaTime)
+		{
+			targetColor = new Color(targetColor.r, targetColor.g, targetColor.b, alpha);
+			alpha = alpha - (1 / duration) * Time.deltaTime;
+			alpha = Mathf.Clamp(alpha, 0f, 1f);
+			yield return null;
+		}
+	}
+
+	private void Update()
+	{
+		if (fadingInProgress) 
+		{
+			faded = fadeStatusInMemory;
+			return;
+		}
+		if (faded && !fadeStatusInMemory) { BeginFadeIn(); }
+		else if (!faded && fadeStatusInMemory) { BeginFadeOut(); }
+	}
+
+}
