9f30536387c13fd10e126ae0a0cdc2cac5f0f0d8
Gamkedo <chris@gamkedo.com>
2020-02-07 20:12:52 -0800

    Credits integrated in main menu

---
+++ Assets/ClickToClosePanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToClosePanel : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
