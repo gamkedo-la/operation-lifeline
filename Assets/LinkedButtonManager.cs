845f2b9c5db2f5072f7b0e73671937312e30878c
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-02-09 10:42:23 -0500

    Bug Fix WIP.  Sound effect skipping on loading screen.  Added waiting coroutine to StartLevel() in LinkedButtonManager.

--- Assets/LinkedButtonManager.cs
+++ Assets/LinkedButtonManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
-
+using System;
 
 public class LinkedButtonManager : MonoBehaviour
 {
@@ -68,7 +68,14 @@ public class LinkedButtonManager : MonoBehaviour
 			TextMeshProUGUI text = loadingButton.GetComponentInChildren<TextMeshProUGUI>();
 			if (text) { text.text = "Loading..."; }
 		}
-		GameManager.Instance.LoadScene(levelToLoad);
         GameManager.Instance.uiSoundOnClick();
+        StartCoroutine(WaitForSoundEffectToFinish());
+        GameManager.Instance.LoadScene(levelToLoad);
+        
+    }
+
+    private IEnumerator WaitForSoundEffectToFinish()
+    {
+        yield return new WaitForSeconds(1.2f);// Hacky fix to wait until the sound effect is done playing before loading next level. 
     }
 }
