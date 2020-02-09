412f918bbb8a102d04952d1a1a9962aea07e9820
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-02-08 21:12:12 -0500

    Added Loading Scene to make sure Master Bank loads prior to Title Screen

---
+++ Assets/Scripts/Utilities/LoadMasterBank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using FMODUnity;
+
+public class LoadMasterBank : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    [SerializeField]
+    private string sceneToMoveTo;
+
+    private void Awake()
+    {
+        gameManager = GameManager.Instance;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager.LoadScene(sceneToMoveTo);
+    }
+
+
+
+
+}
